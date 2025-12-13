6b890362ab968506eedaf34f4b7afce646daed33
justtinsamuel <justtinsamuel01@gmail.com>
2025-12-13 15:55:37 +0700

    update deploy

--- BE/config/config.js
+++ BE/config/config.js
@@ -22,7 +22,7 @@ module.exports = {
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
     dialect: "postgres",
-    port: process.env.DATABASE_PORT,
+    port: process.env.PORT,
     dialectOptions: {
       ssl: {
         require: true,
